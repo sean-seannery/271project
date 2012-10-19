e632c8cc17f13acdf86ba0e9b5ec2e2894d3e31a
sean_seannery <im.sean@gmail.com>
2012-10-19 07:43:13 -0700

    csil student directorys are shared, changed the STATS file name for each server so that they arent writing to the same file for testing purposes

--- src/GradeServerThread.java
+++ src/GradeServerThread.java
@@ -5,7 +5,7 @@ public class GradeServerThread extends ServerThread {
 
 	public GradeServerThread(Server psrv, Socket skt) {
 		super(psrv, skt);
-		this.fileName = "GRADES.txt";
+		this.fileName = psrv.getProcessId() + "GRADES.txt";
 	}
 
 }
