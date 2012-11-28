430cc2fd062997b6a583f40ed73b56af3f8fc305
sean-seannery <sean@cs.ucsb.edu>
2012-11-28 00:51:00 -0800

    changed processID to IP address

--- src/Server.java
+++ src/Server.java
@@ -3,12 +3,15 @@
  */
 
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
@@ -22,6 +25,7 @@ public class Server {
 	private int port;
 	private boolean isGradeServer;
 	private boolean isStatServer;
+	private String serverIP;
 
 	private int currentBallotNumber;	
 	private Hashtable<Integer,ArrayList<ServerMessage> > messageHash;
@@ -33,6 +37,7 @@ public class Server {
 	
 
 	public Server() {
+		serverIP = null;
 		currentBallotNumber = 0;
 		port = 3000;
 		isGradeServer = false;
@@ -184,8 +189,21 @@ public class Server {
 		this.currentBallotNumber = currentBallotNumber;		   
 	}
 	
-	public long getProcessId() {
-		return Thread.currentThread().getId();	
+	public String getProcessId() {
+		if (serverIP == null) {
+		
+			try {
+				URL whatismyip = new URL("http://automation.whatismyip.com/n09230945.asp");
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+				                whatismyip.openStream()));
+	
+				serverIP = in.readLine(); //you get the IP as a String
+			} catch(Exception e) {
+				e.printStackTrace();
+				serverIP = null;
+			}
+		}
+		return serverIP;
 	}
 	
 }
