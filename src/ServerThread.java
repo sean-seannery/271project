c2b7bae3dfa61ba6f16bfcb7a9380c490c51eb68
Stephanie Smith <sns@umail.ucsb.edu>
2012-12-03 18:27:26 -0800

    added hash6

--- src/ServerThread.java
+++ src/ServerThread.java
@@ -58,7 +58,7 @@ public abstract class ServerThread extends Thread{
                 		parentServer.setPaxosLeader(true);
 	                	for (int i = 0; i < this.peerServers.size(); i++){
 		        			//ServerMessage leaderMsg = new ServerMessage(ServerMessage.PAXOS_ADD_LEADER, socket.getLocalAddress().getCanonicalHostName(), socket.getLocalAddress().getCanonicalHostName() );
-                            System.out.println("Is this the right string: " + pHash.get(socket.getInetAddress().toString()) );
+                            System.out.println("Is this the right string: " + pHash.get(socket.getLocalAddress().getCanonicalHostName()) );
                             ServerMessage leaderMsg = new ServerMessage(ServerMessage.PAXOS_ADD_LEADER, pHash.get(socket.getInetAddress().toString())  , pHash.get(socket.getInetAddress().toString())  );
 			        		sendMessage(this.peerServers.get(i), 3000, leaderMsg);
 			        	}
